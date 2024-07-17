a40f908cadb7f12e52a724dc40854bbf746920bd
Harsh <harsh.valecha@yahoo.com>
2024-07-17 22:07:34 +0530

    mean , median , mode

--- pend.py
+++ pend.py
@@ -22,28 +22,34 @@ values = list(zip(df.index,df['Month'],df['Temperature']))
 
 
 # writing the data to excel file
-wb = openpyxl.load_workbook('temp.xlsx')
-wb.create_sheet('temp')
-print(wb.sheetnames)
+# wb = openpyxl.load_workbook('temp.xlsx')
+# wb.create_sheet('temp')
+# print(wb.sheetnames)
 
-sheet = wb.get_sheet_by_name('temp')
+# sheet = wb.get_sheet_by_name('temp')
 
-sheet.cell(row=1,column=1).value = 'S.No'
-sheet.cell(row=1,column=2).value = 'Month'
-sheet.cell(row=1,column=3).value='Temperature'
+# sheet.cell(row=1,column=1).value = 'S.No'
+# sheet.cell(row=1,column=2).value = 'Month'
+# sheet.cell(row=1,column=3).value='Temperature'
 
-for i in range(len(values)):
-    print(values[i][0],values[i][1],values[i][2])
-    # adding serial number
-    sheet.cell(row=i+2,column=1).value = values[i][0]
-    # adding month
-    sheet.cell(row=i+2,column=2).value= values[i][1]
-    # adding temperature
-    sheet.cell(row=i+2,column=3).value= values[i][2]
+# for i in range(len(values)):
+#     print(values[i][0],values[i][1],values[i][2])
+#     # adding serial number
+#     sheet.cell(row=i+2,column=1).value = values[i][0]
+#     # adding month
+#     sheet.cell(row=i+2,column=2).value= values[i][1]
+#     # adding temperature
+#     sheet.cell(row=i+2,column=3).value= values[i][2]
 
-wb.save('temp.xlsx')
+# wb.save('temp.xlsx')
+
+# calculating mean of temperature
+# print(df['Temperature'].mean )
+
+# # median
+# print(df['Temperature'].median)
 
 
-
-
+# print(df['Month'].median)
+# print(df['Temperature'].mode)
 
