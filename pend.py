0906bc7d3fb6a8cd562af9076b7d9b493508cac0
Harsh <harsh.valecha@yahoo.com>
2024-07-17 21:52:03 +0530

    saved the csv values to temp sheet in temp.xlsx

--- pend.py
+++ pend.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import openpyxl
 
 file = pd.read_csv('practice.csv')
 df = pd.DataFrame(data=file)
@@ -17,8 +18,30 @@ df = pd.DataFrame(data=file)
 
 # storing the month and temperatures into list of tuples 
 values = list(zip(df.index,df['Month'],df['Temperature']))
-print(values[:5]) # first five values
+# print(values[:5]) # first five values
 
+
+# writing the data to excel file
+wb = openpyxl.load_workbook('temp.xlsx')
+wb.create_sheet('temp')
+print(wb.sheetnames)
+
+sheet = wb.get_sheet_by_name('temp')
+
+sheet.cell(row=1,column=1).value = 'S.No'
+sheet.cell(row=1,column=2).value = 'Month'
+sheet.cell(row=1,column=3).value='Temperature'
+
+for i in range(len(values)):
+    print(values[i][0],values[i][1],values[i][2])
+    # adding serial number
+    sheet.cell(row=i+2,column=1).value = values[i][0]
+    # adding month
+    sheet.cell(row=i+2,column=2).value= values[i][1]
+    # adding temperature
+    sheet.cell(row=i+2,column=3).value= values[i][2]
+
+wb.save('temp.xlsx')
 
 
 
